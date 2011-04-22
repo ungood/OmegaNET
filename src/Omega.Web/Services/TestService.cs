b09c0011c37c23d7664629668358aa529aa96f76
Jason Walker <ungood@onetrue.name>
2011-04-22 08:21:44 -0600

    refactoring

---
+++ src/Omega.Web/Services/TestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Omega.Web.Services
+{
+    public interface ITestService {}
+
+    public class TestService : ITestService
+    {
+        public string GetSomething()
+        {
+            return "Hello";
+        }
+    }
+}
